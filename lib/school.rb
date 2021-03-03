7339329a6a335944ad371b627d04dbe4aced4d6d
angelo <aurtula1@gmail.com>
2021-03-03 14:16:14 -0500

    commit tree 7b352f6255d8a00c34077d2998b620ab8508c06e

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+    
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << "#{name}"
+    end
+
+    def grade(school_grade)
+        @roster[school_grade]
+    end
+
+    def sort
+        @roster.each do |grade, name|
+            name.sort!
+        end
+    end
+end
